6e05e3449b9627a5f62936c3424d129e60373579
Ricardo Rauch <rickyrauch@gmail.com>
2013-10-08 19:02:02 -0300

    fixes

--- options.php
+++ options.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * A unique identifier is defined to store the options in the database and reference them from the theme.
  * By default it uses the theme name, in lowercase and without spaces, but this can be changed if needed.
